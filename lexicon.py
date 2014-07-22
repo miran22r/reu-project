fb1feeacaced8bc122709bb94cf0c779a7ccdb28
AdolpheJ <jadolphe@uga.edu>
2014-07-22 12:37:35 -0400

    Update lexicon.py

--- lexicon.py
+++ lexicon.py
@@ -1,48 +1,29 @@
-from sentiwordnet import SentiWordNetCorpusReader, SentiSynset
 from nltk.corpus import stopwords
 import numpy as np
+from reader import *
 
 class lexicon:
 
 	def __init__(self):
-		self.swn_filename = 'SentiWordNet.txt'
-		self.swn = SentiWordNetCorpusReader(self.swn_filename)
+		self.read = reader()
 		self.cachedStopWords = stopwords.words("english")
 		f = open('example.txt', mode = 'r')
 		self.files = f.readlines()
-		self.pos_sentiment = 0
-		self.neg_sentiment = 0
 		self.words = []
 		self.tweets = []
-
-	def load_files(self, line):
-		self.words = []
-		self.tweets = self.files
-		line = line.lower()
-		line = line.translate(None, '.')
-		line = line.translate(None, ',')
-		line = line.translate(None, '__')
-		line = line.translate(None, ':')
-		line = line.split()
-		self.words.extend(line)
-			
+	
 	def find_sentiment(self):
-		pos_value = 0
-		neg_value = 0
 		for line in self.files:
-			load_files(line)	
-			pos_value = 0
-			neg_value = 0
-			for word in self.line:
-				if self.swn.senti_synsets(word):
-					if word not in self.cachedStopWords:
-						word1 = self.swn.senti_synsets(word)[0]
-						self.pos_sentiment = self.pos_sentiment + word1.pos_score
-						self.neg_sentiment = self.neg_sentiment + word1.neg_score
-			
-			print "pos " + str(self.pos_sentiment)
-			print "neg " + str(self.neg_sentiment)
-			if self.pos_sentiment > self.neg_sentiment:
-				print "This tweet is pos: " + str(self.pos_sentiment)
-			else:
-				print "This tweet is neg: " + str(self.neg_sentiment)
+			line = line.lower()
+			line = line.translate(None, ',')
+			line = line.translate(None, '.')
+			line = line.translate(None, '!')
+			line = line.translate(None, '"')
+			self.words = line.split()
+			for word in self.words:
+			#	if word not in self.cachedStopWords:
+				self.read.get_scores(word)
+			print line
+			print self.read.get_total_sent()
+			self.read.reset()
+			print "--------------------------"
