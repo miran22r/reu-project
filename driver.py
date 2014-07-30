a8dd5908f668a16f0950ed2a3bcc49fca0ee2597
AdolpheJ <jadolphe@uga.edu>
2014-07-30 10:54:50 -0400

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,8 +1,12 @@
 from lexicon import *
 from plot import *
+from formatter import *
 
 #wordList = lexicon()
 #wordList.find_sentiment()
+
+#formatter = formatter()
+#formatter.format_tweets()
 plotter = plot()
 plotter.graph_sentiment()
 plotter.graph_stock()
