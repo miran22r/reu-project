33e29c015cd6761cd7421219a76b7ce9ba40f949
AdolpheJ <jadolphe@uga.edu>
2014-07-30 15:24:21 -0400

    Update formatter.py

--- formatter.py
+++ formatter.py
@@ -1,31 +1,42 @@
-from nltk.corpus import stopwords
 from pygeocoder import Geocoder
 import numpy as np
 from reader import *
+from geo import *
+import os
 
 class formatter:
 	
 	def __init__(self):
+		self.locations = []
+		self.times = []
+		self.scores = []
+		self.locate = geo()
 		self.read = reader()
-		self.fileName = open('bkw.txt', mode = 'r')
+		self.fileName = open('sbux.txt', mode = 'r')
 		self.fileName = iter(self.fileName)
-		self.newFile = open('burgerking1.txt', mode = 'a')
+		self.newFile = open('starbucks.txt', mode = 'a')
 
 	def format_tweets(self):
 		for line in self.fileName:
 			line = line.lower()
 			if "***" in line:
-				self.newFile.write(line)
-				line = next(self.fileName)
-				try:
-					self.newFile.write(str(Geocoder.geocode(line)))
-					self.newFile.write("\n")
-				except: 
-					self.newFile.write("none")
-					self.newFile.write("\n")
+				line2 = next(self.fileName)
+			#	print "word being passed in " + str(line2)
+			#	print self.locate.coordinates(line2)
+				if 'None' not in self.locate.coordinates(line2):
+			#		print line
+					self.times.append(line)
+					self.locations.append(str(self.locate.coordinates(line2)))
+					line = next(self.fileName)
 				if "###" in line:
 					for word in line.split():
-						read.get_scores(word)
-					self.newFile.write(str(self.read.get_total_sent()))
-					self.newFile.write("\n")
+						self.read.get_scores(word)
+					self.scores.append(str(self.read.get_total_sent()))
 					self.read.reset()
+		for x, y, z in zip(self.scores, self.locations, self.times):
+			self.newFile.write(x)
+			self.newFile.write(y)
+			self.newFile.write(z)
+		self.newFile.write(self.scores)
+		self.newFile.write(self.locations)
+		self.newFile.write(self.times)
