95df32d2368e0a0bd0f5002fcb92d6abbe4a3142
AdolpheJ <jadolphe@uga.edu>
2014-08-02 01:14:40 -0400

    Update formatter.py

--- formatter.py
+++ formatter.py
@@ -7,10 +7,12 @@ import os
 class formatter:
 	
 	def __init__(self):
+		self.counter = 0
 		self.locations = []
 		self.times = []
 		self.scores = []
 		self.locate = geo()
+	#	self.locate.load_file()
 		self.read = reader()
 		self.fileName = open('sbux.txt', mode = 'r')
 		self.fileName = iter(self.fileName)
@@ -18,25 +20,27 @@ class formatter:
 
 	def format_tweets(self):
 		for line in self.fileName:
+			self.newFile = open('starbucks.txt', mode = 'a')
 			line = line.lower()
 			if "***" in line:
 				line2 = next(self.fileName)
-			#	print "word being passed in " + str(line2)
-			#	print self.locate.coordinates(line2)
 				if 'None' not in self.locate.coordinates(line2):
-			#		print line
-					self.times.append(line)
-					self.locations.append(str(self.locate.coordinates(line2)))
-					line = next(self.fileName)
+					try:
+						self.newFile.write(line)
+						self.newFile.write(str(self.locate.coordinates(line2)))
+						self.newFile.write("\n")
+						line = next(self.fileName)
+					except Exception, e:
+						print type(e)
+						print str(e)
+
+
 				if "###" in line:
 					for word in line.split():
-						self.read.get_scores(word)
-					self.scores.append(str(self.read.get_total_sent()))
+						self.read.get_scores(word)	
+					self.newFile.write(str(self.read.get_total_sent()))
+					self.newFile.write("\n")
+					self.counter = self.counter +1
+					print self.counter
 					self.read.reset()
-		for x, y, z in zip(self.scores, self.locations, self.times):
-			self.newFile.write(x)
-			self.newFile.write(y)
-			self.newFile.write(z)
-		self.newFile.write(self.scores)
-		self.newFile.write(self.locations)
-		self.newFile.write(self.times)
+			self.newFile.close()
